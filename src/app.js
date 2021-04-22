d7949dd1ec71f131838c3b09ba8896e6dcebf6b0
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-04-23 00:05:50 +0900

    Add root(/), create, and 'Not found' pages

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 var http = require('http');
+var url = require('url');
 var config = require('./config');
+var template = require('./lib/template');
 
 http.createServer(function (req, resp) {
-    resp.writeHead(200, {'Content-Type': 'text/plain'});
-    resp.write('Hello world!');
-    resp.end('');
+    var pathname = url.parse(req.url).pathname;
+    var htmlTemplate = '';
+    var status = 200;
+
+    if (pathname === '/') 
+        htmlTemplate = template.root;
+    else if (pathname === '/create')
+        htmlTemplate = template.create;
+    else {
+        status = 404;
+        htmlTemplate = 'Not found';
+    }
+
+    resp.writeHead(status);
+    resp.end(htmlTemplate);
 }).listen(config.port);
