3b984a612c3c3877e6464026ebe6eb6a3248089a
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-04-28 21:12:36 +0900

    Add a behavior for '/create_process'

--- src/app.js
+++ src/app.js
@@ -2,21 +2,30 @@ var http = require('http');
 var url = require('url');
 var config = require('./config');
 var template = require('./lib/template');
+var process = require('./lib/process');
 
 http.createServer(function (req, resp) {
     var pathname = url.parse(req.url).pathname;
     var htmlTemplate = '';
     var status = 200;
+    var statusMsg = undefined;
 
     if (pathname === '/') 
         htmlTemplate = template.root;
     else if (pathname === '/create')
         htmlTemplate = template.create;
+    else if (pathname === '/create_process') {
+        process.create(req, resp);
+        status = 302;
+        statusMsg = {
+            'Location': '/'
+        };
+    }
     else {
         status = 404;
         htmlTemplate = 'Not found';
     }
 
-    resp.writeHead(status);
+    resp.writeHead(status, statusMsg);
     resp.end(htmlTemplate);
 }).listen(config.port);
