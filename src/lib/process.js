f14e395d758a9bc9045fecf209fca3f641d7c802
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-04-29 23:59:12 +0900

    Define a function for '/update_process'

--- src/lib/process.js
+++ src/lib/process.js
@@ -13,11 +13,29 @@ module.exports = {
         });
         req.on('end', function() {
             var post = qs.parse(body);
-            var path = `${config.dataDir}/${post.title}.txt`
+            var path = `${config.dataDir}/${post.title}.txt`;
             fs.writeFile(path, post.desc, function (err) {
                 if (err) return console.log(err);
                 console.log(`Write a file (${post.title}: ${post.desc})`);
             });
+        });
+    },
+    update: function(req) {
+        var body = '';
+        req.on('data', function(chunk) {
+            body += chunk;
+
+            if (body.length > 1e6)
+                req.connection.destroy();
+        });
+        req.on('end', function() {
+            var post = qs.parse(body);
+            var path = `${config.dataDir}/${post.title}.txt`;
+            fs.writeFile(path, post.desc, function(err) {
+                if (err) return console.log(err);
+                console.log(`Update the file "${post.title}.txt"`);
+            });
+            console.log(post);
         });
     }
 };
