ed7424864a016373c78fa2fb4ff4c5a95bb58a57
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-04-29 23:42:04 +0900

    Create 'Update' template and add 'Update' link in the root page

--- src/lib/template.js
+++ src/lib/template.js
@@ -30,12 +30,15 @@ function makeHtmlPostList() {
 module.exports = {
     root: function(id) {
         var htmlContent = '<h2>Welcome to My Node.js Web App</h2>';
+        var hrefList = '<a href="/create">Create</a><br>';
+
         if (id !== undefined) {
             htmlContent += '<p>';
             var postPath = `${config.dataDir}/${id}.txt`;
             if (fs.existsSync(postPath)) {
-                var postDesc = fs.readFileSync(postPath);
+                var postDesc = fs.readFileSync(postPath, 'utf-8');
                 htmlContent += postDesc;
+                hrefList += `<a href="/update?id=${id}">Update</a>`;
             }
             else
                 htmlContent += 'No post was found.';
@@ -46,9 +49,7 @@ module.exports = {
         <p>${htmlContent}</p>
         <h3>Posts</h3>
         <p>${makeHtmlPostList()}</p>
-        <p>
-            <a href="/create">Create</a>
-        </p>
+        <p>${hrefList}</p>
         `);
     },
     create: function() {
@@ -62,6 +63,25 @@ module.exports = {
             </p>
             <p><input type="submit"></p>
         </form>
-    `);
+        `);
     },
+    update: function(id) {
+        var postPath = `${config.dataDir}/${id}.txt`;
+        var postDesc = fs.readFileSync(postPath, 'utf-8');
+        console.log(postDesc);
+        return makeHtmlTemplate(`
+        <form action="http://localhost:${config.port}/update_process" 
+        method="post">
+            <p>
+            <input type="hidden" id="id" name="id" value=${id}>
+            <input type="text" id="title" name="title" placeholder="Title",
+             value="${id}">
+            </p>
+            <p><textarea id="desc" name="desc" 
+                placeholder="Description">${postDesc}</textarea>
+            </p>
+            <p><input type="submit"></p>
+        </form>
+        `);
+    }
 };
